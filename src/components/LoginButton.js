505d53c5fd1cb45100166549e1e2206cd99fd963
sirphemmiey <oluwafemiakinde@gmail.com>
2018-08-17 13:49:01 -0800

    LoginButton

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Button, IconButton, Menu, MenuItem, ListItemText } from '@material-ui/core';
-import { AccountCircle } from '@material-ui/icons';
-import { withAuth } from '@okta/okta-react';
+import {
+    Button,
+    IconButton,
+    Menu,
+    MenuItem,
+    ListItemText
+} from '@material-ui/core';
+import {
+    AccountCircle
+} from '@material-ui/icons';
+import {
+    withAuth
+} from '@okta/okta-react';
 
 class LoginButton extends React.Component {
     state = {
         authenticated: null,
         user: null,
-        menuAnchor: null
+        menuAnchorEl: null
     };
 
     componentDidUpdate() {
@@ -15,5 +25,58 @@ class LoginButton extends React.Component {
     }
     async componentDidCatch() {
         const authenticated = this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+            const user = await this.props.auth.getUser();
+            this.setState({
+                authenticated,
+                user
+            });
+        }
+    }
+    login = () => {
+        this.props.auth.login();
+    }
+    logout = () => {
+        this.handleMenuClose();
+        this.props.auth.logout();
+    };
+    handleMenuOpen = event => this.setState({
+        menuAnchorEl: event.currentTarget
+    });
+    handleMenuClose = () => {
+        this.setState({
+            menuAnchorEl: null
+        });
+    }
+
+    render() {
+        const { authenticated, user, menuAnchorEl } = this.state;
+        if (authenticated == null) {
+            return null;
+        }
+        if (!authenticated) {
+            return <Button color="inherit" onClick={this.login}>Login</Button>
+        }
+        const menuPosition = {
+            vertical: 'top',
+            horizontal: 'right'
+        };
+
+        return (
+            <div>
+                <IconButton onClick={this.handleMenuOpen} color="inherit">
+                    <AccountCircle />
+                </IconButton>
+                <Menu anchorEl={menuAnchorEl}
+                open={!!menuAnchorEl}
+                onClose={this.handleMenuClose}>
+                <MenuItem onClick={this.logout}>
+                    <ListItemText primary="Logout" secondary={user && user.name} />
+                </MenuItem>
+                </Menu>
+            </div>
+        );
     }
 }
+
+export default withAuth(LoginButton);
